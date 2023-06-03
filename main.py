732e8267a6aafee6fe0c23a5ac76876fd9907474
dori-dev <mr.dori.dev@gmail.com>
2023-06-03 20:13:06 +0330

    insert & select data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import and_
 
 # database = 'postgresql'
 # library = 'library'
@@ -15,6 +16,7 @@ from sqlalchemy import Column, Integer, String
 
 engine = create_engine('sqlite:///database.db')
 base = declarative_base()
+session = sessionmaker(bind=engine, autocommit=True)()
 
 
 class Student(base):
@@ -24,3 +26,23 @@ class Student(base):
 
 
 base.metadata.create_all(engine)
+
+# select
+students = session.query(Student).order_by(Student.id).all()
+for student in students:
+    print(student.id, student.name)
+
+students = session.query(Student).filter(Student.name == 'ali')
+print(students.count())
+students = session.query(Student).filter(
+    and_(Student.name == 'ali', Student.id == 2)
+)
+
+# insert
+s1 = Student(name='ali')
+# session.add(s1)
+# session.commit()
+
+s2 = Student(name='saman')
+# session.add_all([s1, s2])
+# session.commit()

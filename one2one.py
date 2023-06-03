721583838046e0e437e79d3992b01ba814ccbd4e
dori-dev <mr.dori.dev@gmail.com>
2023-06-03 22:44:52 +0330

    use one to one relation

--- one2one.py
+++ one2one.py
@@ -4,36 +4,34 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 
 
-engine = create_engine('sqlite:///school.db')
+engine = create_engine('sqlite:///country.db')
 base = declarative_base()
 session = sessionmaker(bind=engine)()
 
 
-class Student(base):
-    __tablename__ = 'student'
+class Country(base):
+    __tablename__ = 'country'
     id = Column('id', Integer, unique=True, primary_key=True)
     name = Column('name', String(50))
-    classroom_id = Column('classroom_id', Integer, ForeignKey('classroom.id'))
+    capital = relationship('Capital', backref='country', uselist=False)
 
 
-class ClassRoom(base):
-    __tablename__ = 'classroom'
+class Capital(base):
+    __tablename__ = 'capital'
     id = Column('id', Integer, unique=True, primary_key=True)
     name = Column('name', String(50))
-    students = relationship('Student', backref='classroom')
+    country_id = Column('country_id', Integer, ForeignKey('country.id'))
 
 
 base.metadata.create_all(engine)
 
 
-c1 = ClassRoom(name='class a')
-session.add(c1)
+iran = Country(name='Iran')
+session.add(iran)
 session.commit()
 
-s1 = Student(name='ali', classroom=c1)
-s2 = Student(name='mmd', classroom=c1)
-session.add_all([s1, s2])
+tehran = Capital(name='Tehran', country=iran)
+session.add(iran)
 session.commit()
 
-for student in c1.students:
-    print(student.name)
+print(session.query(Country).first().capital.name)
